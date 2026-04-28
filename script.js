fbbcfe32394ab6d337f43ab5bf2acdfafb82b250
Temmie Loluwa <falusipeace4@gmail.com>
2026-04-28 15:41:27 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -133,3 +133,20 @@ modal.onclick = function(event) {
       btn.textContent = "Show Less";
     }
   });
+
+  const backToTop = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+
+  backToTop.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  });
